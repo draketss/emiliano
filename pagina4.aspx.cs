2836941b36fe737ab15ea1d1c675175585b7353f
draketss <110877231+draketss@users.noreply.github.com>
2024-05-20 20:05:11 -0600

    Add files via upload

---
+++ pagina4.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pagina_Maestra
+{
+    public partial class pagina4 : System.Web.UI.Page
+    {
+        protected void ButtonSubmit_Click(object sender, EventArgs e)
+        {
+            // Mostrar un mensaje de agradecimiento al enviar la encuesta
+            LabelMessage.Visible = true;
+            LabelMessage.Text = "¡Gracias por completar la encuesta!";
+        }
+    }
+}
